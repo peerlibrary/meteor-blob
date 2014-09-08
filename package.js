b4736e3920293a5e2bcf1a1d67fcc5a5ac4bc5ab
Mitar <mitar.git@tnode.com>
2014-09-08 13:04:32 -0700

    Version bump.

--- package.js
+++ package.js
@@ -8,7 +8,7 @@
 
 Package.describe({
   summary: "Polyfill for Blob to use in tests in PhantomJS",
-  version: '0.1.1',
+  version: '0.1.2',
   name: 'peerlibrary:blob',
   git: 'https://github.com/peerlibrary/meteor-blob.git'
 });
